246940cecca9beee07f5b0ab54c2e5255fe254f2
Rob Jones <rcj@rcj.io>
2017-12-29 15:58:33 -0600

    broke out geo math into a seperate function and did some commenting

--- graphics.go
+++ graphics.go
@@ -19,17 +19,28 @@ func round(raw float64) (rounded int) {
 	return int(math.Floor(raw + .5))
 }
 
-func drawAirport(runways []*aptdata.Runway, code string, name string, city string, region string, country string) {
-	endpoints := make([][2]*geo.Point, len(runways))
-	var minLatitude, maxLatitude, minLongitude, maxLongitude float64
-	var nePoint, nwPoint, sePoint, swPoint *geo.Point
+// This math is super-duper ugly and was iteratively developed.  I do not
+// profess strength in geometry, and there could totally be a better way
+// of doing this.   There probably is.   I'm st00pid.  But this works.
+func calcPixels(runways []*aptdata.Runway) (pxEndpoints [][2][2]float64, xDimension int, yDimension int) {
+	degEndpoints := make([][2]*geo.Point, len(runways))
+	pxEndpoints = make([][2][2]float64, len(runways))
+	var minLatitude, maxLatitude, minLongitude, maxLongitude,
+		xyLengthRatio, xLengthDegrees, yLengthDegrees, lngAdjFactor,
+		latAdjFactor float64
 
-	// Set some values we know will be reset by our data
+	// Set some values we know will be reset by our data to ensure we properly
+	// capture all the extremes.  (Setting, say, minLatitude to 0 won't work,
+	// because we may have negative latitudes -- instead we'll set it as high
+	// as it could possibly be and let our data drag it down.)
 	minLatitude = 90
 	maxLatitude = -90
 	minLongitude = 180
 	maxLongitude = -180
 
+	// Find our minimum and maximum lats and longs so we know the
+	// dimensions of our bounding box.  Then create a slice of pairs of
+	// points representing the ends of each runway.
 	for i, r := range runways {
 		minLatitude = math.Min(minLatitude,
 			math.Min(r.End1Latitude, r.End2Latitude))
@@ -40,52 +51,70 @@ func drawAirport(runways []*aptdata.Runway, code string, name string, city strin
 		maxLongitude = math.Max(maxLongitude,
 			math.Max(r.End1Longitude, r.End2Longitude))
 
-		endpoints[i] = [2]*geo.Point{geo.NewPoint(r.End1Latitude, r.End1Longitude),
+		degEndpoints[i] = [2]*geo.Point{geo.NewPoint(r.End1Latitude, r.End1Longitude),
 			geo.NewPoint(r.End2Latitude, r.End2Longitude)}
 	}
 
-	nwPoint = geo.NewPoint(maxLatitude, minLongitude)
-	nePoint = geo.NewPoint(maxLatitude, maxLongitude)
-	swPoint = geo.NewPoint(minLatitude, minLongitude)
-	sePoint = geo.NewPoint(minLatitude, maxLongitude)
+	// Create a point for each corner of the bounding box
+	nwPoint := geo.NewPoint(maxLatitude, minLongitude)
+	nePoint := geo.NewPoint(maxLatitude, maxLongitude)
+	swPoint := geo.NewPoint(minLatitude, minLongitude)
+	sePoint := geo.NewPoint(minLatitude, maxLongitude)
 
-	// TODO: Correct this for the slight difference in distance between
-	// lines as well?
-	xLongDistance := maxLongitude - minLongitude
-	yLatDistance := maxLatitude - minLatitude
+	// Find the lat/long deltas for the X and Y sides.  TODO: Correct this for
+	// the slight difference in distance betweenlines like we do below? Meh.
+	xLengthDegrees = maxLongitude - minLongitude
+	yLengthDegrees = maxLatitude - minLatitude
 
-	xDistance := round(kmToFeet(math.Max(nwPoint.GreatCircleDistance(nePoint),
+	// Determine what the length of each side is in feet.  We look for the max
+	// here to offset the fact that one degree will have slightly different
+	// lengths depending on where in the world you are.  This is probably overly
+	// picky.
+	xLengthFeet := round(kmToFeet(math.Max(nwPoint.GreatCircleDistance(nePoint),
 		swPoint.GreatCircleDistance(sePoint))))
-	yDistance := round(kmToFeet(math.Max(nwPoint.GreatCircleDistance(swPoint),
+	yLengthFeet := round(kmToFeet(math.Max(nwPoint.GreatCircleDistance(swPoint),
 		nePoint.GreatCircleDistance(sePoint))))
 
-	var xyDistanceRatio float64
-	xyDistanceRatio = float64(xDistance) / float64(yDistance)
+	// Determine the ratio of the LengthDegreess of the north/south and
+	// east/west sides.  This will be used to scale the actual pixel-counts of
+	// the sides.
+	xyLengthRatio = float64(xLengthFeet) / float64(yLengthFeet)
 
-	var xDimension, yDimension int
-
-	if xDistance > yDistance {
+	// Set the dimension of each side in pixels witn the longest side being
+	// defined by ChartSideLength and the shorter by the xyLengthRatio.
+	if xLengthFeet > yLengthFeet {
 		xDimension = ChartSideLength
-		yDimension = round(ChartSideLength / xyDistanceRatio)
+		yDimension = round(ChartSideLength / xyLengthRatio)
 	} else {
 		yDimension = ChartSideLength
-		xDimension = round(ChartSideLength * xyDistanceRatio)
+		xDimension = round(ChartSideLength * xyLengthRatio)
 	}
 
-	lngAdjFactor := float64(xDimension) / xLongDistance
-	latAdjFactor := float64(yDimension) / yLatDistance
+	// Eacn axis needs to have its ration of distance in degrees to distance
+	// in pixels adjusted seperately, due to the difference in the actual
+	// size of a "degree" as mentioned above.  This will create a (faily large)
+	// float that we multiply each degree delta by to convert it into pixels.
+	lngAdjFactor = float64(xDimension) / xLengthDegrees
+	latAdjFactor = float64(yDimension) / yLengthDegrees
 
-	adjEndpoints := make([][2][2]float64, len(runways))
-	for i, r := range endpoints {
-		adjEndpoints[i] = [2][2]float64{{float64(round((r[0].Lat() - minLatitude) * latAdjFactor)),
+	// Now we create a new array, witn the delta degrees converted into pixel
+	// coordinates as descibed above.
+	for i, r := range degEndpoints {
+		pxEndpoints[i] = [2][2]float64{{float64(round((r[0].Lat() - minLatitude) * latAdjFactor)),
 			float64(round((r[0].Lng() - minLongitude) * lngAdjFactor))},
 			{float64(round((r[1].Lat() - minLatitude) * latAdjFactor)),
 				float64(round((r[1].Lng() - minLongitude) * lngAdjFactor))}}
 	}
 
-	// DRAWING TIME!
+	return pxEndpoints, xDimension, yDimension
+}
 
-	// define the colors
+func drawAirport(runways []*aptdata.Runway, code string, name string, city string, region string, country string) {
+	// Get our runway endpoints and image size as pixels.  The math is ugly,
+	// so it's contained in a seperate function.
+	pxEndpoints, xDimension, yDimension := calcPixels(runways)
+
+	// define the colors for easier setting
 	blueprint := color.RGBA{4, 63, 140, 255}
 	white := color.RGBA{255, 255, 255, 255}
 
@@ -97,11 +126,12 @@ func drawAirport(runways []*aptdata.Runway, code string, name string, city strin
 	canvas.SetColor(blueprint)
 	canvas.Clear()
 
+	// Prepare for line drawing
 	canvas.SetColor(white)
 	canvas.SetLineWidth(3)
 
 	// Draw a line for each runway
-	for _, p := range adjEndpoints {
+	for _, p := range pxEndpoints {
 		canvas.DrawLine(p[0][1], p[0][0],
 			p[1][1], p[1][0])
 	}
@@ -125,9 +155,14 @@ func drawAirport(runways []*aptdata.Runway, code string, name string, city strin
 	// Time to do some labelling!
 	canvas.LoadFontFace("flux.ttf", 12)
 	canvas.SetLineWidth(3)
-	// TODO: Get these from the database
+
+	// Set strings for our name and location.  Sometimes the city is empty
+	// in our data.  Handle that neatly.
 	nameCode := fmt.Sprintf("%s (%s)", name, code)
-	location := fmt.Sprintf("%s, %s, %s", city, region, country)
+	location := fmt.Sprintf("%s, %s", region, country)
+	if city != "" {
+		location = fmt.Sprintf("%s, %s", city, location)
+	}
 
 	// Get the dimensions of our text
 	nWidth, nHeight := canvas.MeasureString(nameCode)
@@ -139,27 +174,14 @@ func drawAirport(runways []*aptdata.Runway, code string, name string, city strin
 	textWidth := math.Max(nWidth, lWidth)
 	boxWidth := textWidth + float64(textMargin*2)
 	boxHeight := nHeight + lHeight + float64(textMargin*2+lineSpacing)
-	fmt.Println(boxHeight)
 
 	// Clear out a box for the text to fit into
-	//var boxX, boxY float64
-	var boxX float64
-	boxX = float64(SideLength - boxWidth)
-	//boxY = float64(boxHeight)
+	boxX := float64(SideLength - boxWidth)
 	canvas.DrawRectangle(boxX, 0, boxWidth, boxHeight)
 	canvas.SetColor(blueprint)
 	canvas.Fill()
 
-	// And put a border on it
-	/*canvas.DrawLine(boxX, 0, boxX, boxY)
-	canvas.DrawLine(boxX, boxY, SideLength, boxY)
-	canvas.SetColor(white)
-	canvas.Stroke()
-	*/
-	//	canvas.SetRGB(0.016, 0.246, 0.547)
-	//canvas.DrawRectangle(0, 0, 200, 200)
-	//canvas.Fill()
-
+	// Add the text
 	canvas.SetColor(white)
 	canvas.DrawString(nameCode, SideLength-textMargin-nWidth, nHeight+textMargin)
 	canvas.DrawString(location, SideLength-textMargin-lWidth, nHeight+lHeight+textMargin+lineSpacing)
